dc500b7995a7934fd10727f2869f58d9d0ba172d
Caio Quirino <caioquirino@gmail.com>
2016-06-30 03:12:45 +0000

    Spring Cloud Config partially working

--- config-server/src/main/groovy/com/github/dabuild/config/server/DabuildConfigServerApplication.groovy
+++ config-server/src/main/groovy/com/github/dabuild/config/server/DabuildConfigServerApplication.groovy
@@ -3,10 +3,12 @@ package com.github.dabuild.config.server
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 class DabuildConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DabuildConfigServerApplication.class, args)
